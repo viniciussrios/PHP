97649b06aeaba21bdf39d37563b70623a11b1249
Vinicius Rios <viniciussantosrios@gmail.com>
2020-07-21 20:58:33 -0300

    Corrigido

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Olá Mundo 456!";
 
-	echo "Olá Udemy!";
-=======
-	echo "Olá Mundo123!";
-	
-	echo "Olá Hcode!";
->>>>>>> d2ea925020ce628bec3aa13f389f78811e7f41ba
+	echo "Olá Udemy e Hcode tudo certo!";
+
 
 ?>
